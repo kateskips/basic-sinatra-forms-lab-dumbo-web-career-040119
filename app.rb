2620d1e89140043e25194ef988975f14cf793a6b
Kate F <follett.katie@gmail.com>
2019-04-28 15:07:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
   
-  get '/newteam' do
+   get '/newteam' do
     erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+    erb :team
   end
 
 
